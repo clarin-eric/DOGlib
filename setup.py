0334cc06e999f6ac190f0f12078a1a2b94a6c612
Michal Gawor <michal.j.gawor@gmail.com>
2021-07-12 16:37:09 +0200

    dependency test

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     version='0.1.0',
     description='Digital Object Gate Python library',
     author='Michał Gawor',
-    install_requires=['certifi==2021.5.30', 'lxml==4.6.3', 'pycurl==7.43.0.6'],
+    install_requires=['certifi==2021.5.30', 'pycurl==7.43.0.6'],
+    test_requires=['lxml==4.6.3'],
 )
