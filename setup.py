0696a81d6d0f04ba85d1ab69a0a0c9fcc1d8c6f2
Michal Gawor <michal.j.gawor@gmail.com>
2021-07-14 16:42:50 +0200

    missing commas

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="doglib",
     packages=find_packages(include=["doglib"]),
     version='0.1.0',
-    package_dir={"": "repo_configs"}
-    package_data={"doglib": ["", "*.json"]}
+    package_dir={"": "repo_configs"},
+    package_data={"doglib": ["", "*.json"]},
     include_package_data=True,
     description='Digital Object Gate Python library',
     author='Michał Gawor',
