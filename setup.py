11252a464419446f8b75f1a40108dd1bc35c7386
Michal Gawor <michal.j.gawor@gmail.com>
2021-07-14 16:40:07 +0200

    fix data files in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='doglib',
-    packages=find_packages(include=['doglib']),
+    name="doglib",
+    packages=find_packages(include=["doglib"]),
     version='0.1.0',
-    package_data={'doglib': ['*.json', 'repo_configs/*.json']}
+    package_dir={"": "repo_configs"}
+    package_data={"doglib": ["", "*.json"]}
     include_package_data=True,
     description='Digital Object Gate Python library',
     author='Michał Gawor',
