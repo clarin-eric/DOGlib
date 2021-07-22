968ae4f65d477cbfe6ced889c063c94afc6797ab
Michal Gawor <michal.j.gawor@gmail.com>
2021-07-22 15:49:18 +0200

    working setup structure

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="doglib",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     version='0.1.0',
-    package_dir={"": "repo_configs"},
-    package_data={"doglib": ["", "*.json"]},
+    package_data={"src": ["*.json"]},
     include_package_data=True,
     description='Digital Object Gate Python library',
     author='Michał Gawor',
