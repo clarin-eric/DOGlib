73964f54817a4ab451a50a3be2b9d42ce5f23cc9
Michal Gawor <michal.j.gawor@gmail.com>
2021-07-22 17:04:45 +0200

    simplify project structure

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="doglib",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    version='0.1.0',
-    package_data={"src": ["*.json"]},
-    include_package_data=True,
     packages=["doglib"],
+    version='0.1.0',
+    package_data={"doglib": ["*.json"]},
+    include_package_data=True,
     description='Digital Object Gate Python library',
     author='Michał Gawor',
 )
